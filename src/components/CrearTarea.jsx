f66c7a0442573ef34e9673c0a212858a6536f3d7
Felip3 <juanfeliperamirezcardona0@gmail.com>
2025-05-06 20:21:17 -0500

    se a estilos a los inputs de crear tareas

--- src/components/CrearTarea.jsx
+++ src/components/CrearTarea.jsx
@@ -2,35 +2,53 @@ import { useState } from "react";
 import { agregarTarea } from "../services/apiFake"
 import MostrarTareas from "./MostrarTareas";
 
-export default function CrearTareas (){
+export default function CrearTareas() {
     const [nombre, setNombre] = useState("")
     const [descripcion, setDescripcion] = useState("")
 
     const crearTarea = (e) => {
         e.preventDefault();
-        if (!nombre || !descripcion ) {
+        if (!nombre || !descripcion) {
             alert("verifica los espacios en blanco")
             return;
         } else {
             let usuario = JSON.parse(localStorage.getItem("usuario"));
-            const idUsuario =(usuario.id) 
-            const tarea = {idUsuario, nombre, descripcion, usuario };
+            const idUsuario = (usuario.id)
+            const tarea = { idUsuario, nombre, descripcion, usuario };
             agregarTarea(tarea);
             setNombre("");
             setDescripcion("");
             console.log(tarea);
-            
+
         }
     };
 
-    return(
+    return (
         <div className="contenedorTareas">
             <form action="">
-                <input value={nombre} onChange={(e) => setNombre(e.target.value)} type="text" placeholder="Nombre tarea"/>
-                <input value={descripcion} onChange={(e) => setDescripcion(e.target.value)} type="text" placeholder="Descripcion tarea"/>
-                <button type="button" onClick={crearTarea}>Crear tarea</button>
+                <input
+                    className="inputTarea"
+                    value={nombre}
+                    onChange={(e) => setNombre(e.target.value)}
+                    type="text"
+                    placeholder="Nombre tarea"
+                />
+                <input
+                    className="inputTarea"
+                    value={descripcion}
+                    onChange={(e) => setDescripcion(e.target.value)}
+                    type="text"
+                    placeholder="Descripcion tarea"
+                />
+                <button
+                    className="botonCrearTarea"
+                    type="button"
+                    onClick={crearTarea}
+                >
+                    Crear tarea
+                </button>
                 <MostrarTareas></MostrarTareas>
             </form>
         </div>
-    )
+    );
 }
--- src/pages/ContenedorPrincipal.jsx
+++ src/pages/ContenedorPrincipal.jsx
@@ -2,11 +2,14 @@ import { Link } from 'react-router-dom';
 export default function ContenedorPrincipal() {
     return (
         <div className="contenedorPrincipal">
+            <section className='tarjetaContenedora'>
+
             <h1>Gestor de tareas internas</h1>
             <div className='contenedorBotones'>
                 <Link className='link' to="/registro">REGISTRO</Link>
                 <Link  className='link' to="/tareas">TAREAS</Link>
             </div>
+            </section>
         </div>
     );
 }
